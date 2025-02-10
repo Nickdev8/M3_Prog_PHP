eb283efbd39ed0d3ad47eadac4f51bc367d5eb87
Nick <nick.esselman@gmail.com>
2025-02-10 12:19:54 +0100

    Add initial index.php with sample content and image

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Kopje</h1>";
+echo "<h1>dit is Bram Grippeling:</h1>";
+echo "<img src='images/afbeelding.jpg' alt='Afbeelding' style='width: 400px; height: 200px;'>";
+echo "<h3>AAAAAHHAAAHGGH</h3>";
+echo "<h5>aaa</h5>";
+?>
