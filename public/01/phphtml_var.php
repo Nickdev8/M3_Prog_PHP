8960146ddbc6708688462459d6d9a3aeeef1334b
Nick <nick.esselman@gmail.com>
2025-02-10 12:44:04 +0100

    Add PHP files for dynamic content generation and HTML structure

---
+++ public/01/phphtml_var.php
@@ -0,0 +1,23 @@
+<?php
+    $titel = "Titel van de pagina";
+    $datum = date('d-m-Y');
+    $tijd = date("H:i");
+    $game = "Super metroid";
+
+    echo $titel;
+    echo $datum;
+    echo $tijd;
+    echo $game;
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title><?php echo "Titel van de pagina" ?></title>
+    </head>
+    <body>
+        <h2>De datum vandaag is: <?php echo date('d-m-Y')?>, en de tijd is <?php echo date("H:i")?> uur.
+        
+        <p><h2>Mijn favoriete game</h2><?php echo "Super metroid";?></p>
+    </body>
+</html>
