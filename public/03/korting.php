49996b02cf93fe119711f4ddbc8679adf837234a
Nick <nick.esselman@gmail.com>
2025-02-24 11:52:36 +0100

    Add discount logic and update gift value in korting.php

--- public/03/korting.php
+++ public/03/korting.php
@@ -4,12 +4,17 @@ $totaalBesteld = 100.0;
 $verzendLosten = 3.50;
 $korting = 0;
 $cadeautje = true;
+$cadeautjeWaarde = 10;
 
+if ($totaalBesteld > 1000){
+    $korting += 7.5;
+    $cadeautjeWaarde += 30;
+}
 $totaal = $totaalBesteld - $korting*$totaalBesteld + $verzendLosten;
 
 echo "Totaal Besteld: $totaalBesteld </br>";
 echo "verzendLosten: $verzendLosten </br>";
 
 if ($cadeautje)
-    echo "U krijgt ook een cadeautje ter waarde ban 10 euro: $cadeautje </br>";
+    echo "U krijgt ook een cadeautje ter waarde ban $cadeautjeWaarde euro: $cadeautje </br>";
 ?>
