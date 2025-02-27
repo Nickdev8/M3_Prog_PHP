a91ba6fb1b34fa4718c7fad9bafd0f178c2212fa
Nick <nick.esselman@gmail.com>
2025-02-27 12:07:11 +0100

    Enhance array handling in arrays_eigen.php with additional game and sorting functionality

--- public/04/arrays_eigen.php
+++ public/04/arrays_eigen.php
@@ -1,7 +1,23 @@
 <?php
 $games = ["The Legend of Zelda", "Super Mario", "Metroid", "Donkey Kong", "Kirby", "Star Fox", "F-Zero", "Pikmin", "Animal Crossing", "Splatoon"];
 
-echo $games;
+$games[] = "Never gonna give you up";
+
+//echo $games;
 echo "<br>";
 print_r($games);
+echo "<br>";
 var_dump($games);
+
+
+echo "<br>";
+
+echo count($games). "<br>";
+
+echo "<br>";
+
+echo implode(' >> ', $games);
+echo "<br>";
+sort($games);
+echo implode(' >> ', $games);
+echo "<br>";
