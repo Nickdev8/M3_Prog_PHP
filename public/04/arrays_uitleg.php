ae7de913212383fd387e4116326358598f5884b2
Nick <nick.esselman@gmail.com>
2025-02-27 11:55:13 +0100

    Add PHP script to demonstrate array usage and print elements

---
+++ public/04/arrays_uitleg.php
@@ -0,0 +1,13 @@
+<?php 
+$TheBestArrayEveryToExist = ["Dit", "is", "een", "array", "met", "strings"];
+
+
+$nummers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+print_r($nummers);
+
+echo "<br>";
+echo $nummers[1];
+echo "<br>";
+echo $nummers[5];
+echo "<br>";
