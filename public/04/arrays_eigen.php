b2c24a5cc283b93f71502cf195f9ac80a4722ce7
Nick <nick.esselman@gmail.com>
2025-02-27 12:00:15 +0100

    Add PHP script to demonstrate array handling with game titles

---
+++ public/04/arrays_eigen.php
@@ -0,0 +1,7 @@
+<?php
+$games = ["The Legend of Zelda", "Super Mario", "Metroid", "Donkey Kong", "Kirby", "Star Fox", "F-Zero", "Pikmin", "Animal Crossing", "Splatoon"];
+
+echo $games;
+echo "<br>";
+print_r($games);
+var_dump($games);
